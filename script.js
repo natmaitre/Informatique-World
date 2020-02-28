f8b7442b6811fb193ba74d5bcaef1bb2a5e684fa
natmaitre <natanvenierfr@gmail.com>
2020-02-28 16:35:13 -0600

    add js

---
+++ script.js
@@ -0,0 +1,11 @@
+var lang = "en";
+if (lang === "en") document.getElementById("home_title").innerHTML = "Home";
+
+var lang = "en";
+if (lang === "en") document.getElementById("tel_title").innerHTML = "Downloads";
+
+var lang = "en";
+if (lang === "en") document.getElementById("res_title").innerHTML = "Ressources";
+
+var lang = "en";
+if (lang === "en") document.getElementById("tuto_title").innerHTML = "Tutorials";
