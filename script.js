07aefea5341b381e6cc24b3a40aa2a3dbfe2a7db
natmaitre <natanvenierfr@gmail.com>
2020-02-28 18:20:25 -0600

    change into fr

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var lang = "en";
+var lang = "fr";
 if (lang === "en") document.getElementById("home_title").innerHTML = "Home";
 
-var lang = "en";
+var lang = "fr";
 if (lang === "en") document.getElementById("tel_title").innerHTML = "Downloads";
 
-var lang = "en";
+var lang = "fr";
 if (lang === "en") document.getElementById("res_title").innerHTML = "Ressources";
 
-var lang = "en";
+var lang = "fr";
 if (lang === "en") document.getElementById("tuto_title").innerHTML = "Tutorials";
